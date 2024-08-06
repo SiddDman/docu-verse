6dba90f743f52b41852feba7352bd656e961582c
Siddharth <siddharth12dhiman@gmail.com>
2024-08-06 13:51:18 +0530

    setting up the colors in css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google"
-
 import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-export default function RootLayout({ children }: {children: React.ReactNode}) {
+export const metadata: Metadata = {
+  title: 'DocuVerse',
+  description: 'Your go-to collaborative editor.'
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )
